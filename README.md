253d68f70cd244a2287f1553dbab7e53c9158bfe
monkyyy <crazymonkyyy@gmail.com>
2022-11-22 06:51:01 -0700

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+imma make a factorio mod; the project
+
+I see 3 ways that effect the complexity of a crafting tree, the order of the nodes, the depth of the tree and the wieght of the connections; I only see 2 of these explored and think they are the bad ones
---
+++ prototypes/monkyyy.lua
@@ -0,0 +1,24 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "wooden-chest",
+	 icon = "__base__/graphics/icons/wooden-chest.png",
+	 icon_size = 64,
+	 subgroup = "raw-material",
+    ingredients = {{"wood", 4}},
+    results = {
+		{
+          amount = 1,
+          name = "wooden-chest",
+          probability = 0.99299999999999997
+		},
+		{
+          amount = 1,
+          name = "iron-ore",
+          probability = 0.0070000000000000009
+		}
+	}
+  }
+}
+)
